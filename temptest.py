dc028066fbfcb66a85fc5fbe50c342283589dcf0
Lee <clam@nyanya.org>
2020-06-09 05:56:39 +1000

    Update temptest.py
    typo

--- temptest.py
+++ temptest.py
@@ -4,7 +4,7 @@ PIN = 26
 
 
 hum, temp = Adafruit_DHT.read_retry(SENSOR, PIN)
-if humidity is not None and temperature is not None:
+if hum is not None and temp is not None:
     print("Temp={0:0.1f}*C  Humidity={1:0.1f}%".format(temp, hum))
 else:
     print("Failed...")
