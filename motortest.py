e547a4b4cc932eec979e7fb0dc3b588874ba650c
Lee <clam@nyanya.org>
2020-09-01 17:36:57 +1000

    Create motortest.py

---
+++ motortest.py
@@ -0,0 +1,18 @@
+import time
+from dual_mc33926_rpi import motors, MAX_SPEED
+
+SPEEDS = [0,1,2,3,4,5,6,7,8,9,10,-1,-2,-3,-4,-5,-6,-7,-8,-9,-10]
+
+try:
+    motors.enable()
+    motors.setSpeeds(0, 0)
+
+    print("Motor 1 test")
+    for s in SPEEDS:
+        motors.motor1.setSpeed(s)
+        time.sleep(0.005)
+finally:
+  # Stop the motors, even if there is an exception
+  # or the user presses Ctrl+C to kill the process.
+  motors.setSpeeds(0, 0)
+  motors.disable()
