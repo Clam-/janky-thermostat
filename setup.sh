d639b734c438ea5190bcc5b6f6ac9a34c375e232
Lee <clam@nyanya.org>
2020-11-21 15:44:50 +1100

    Update setup.sh
    Fix EOL mistakes

--- setup.sh
+++ setup.sh
@@ -3,6 +3,6 @@ rrdtool create data/temppos.rrd \
 --step 60 \
 DS:temp:GAUGE:120:-10:55 \
 DS:pos:GAUGE:120:1000:25000 \
-RRA:MAX:0.5:1:2880
-RRA:MAX:0.5:60:730
+RRA:MAX:0.5:1:2880 \
+RRA:MAX:0.5:60:730 \
 RRA:MAX:0.5:240:8760
