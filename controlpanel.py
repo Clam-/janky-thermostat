250488b4f0bae47f815a162150310e8207ed7f0e
Clam- <clam@nyanya.org>
2023-08-21 02:47:08 +1000

    Update controlpanel.py
    more mistake.

--- controlpanel.py
+++ controlpanel.py
@@ -2,7 +2,8 @@ import sqlite3
 from flask import g, Flask, render_template, request
 
 DATABASE = 'settings.sqlite'
-ROW_UPDATE = "UPDATE setting SET :target_temp,:last_position,:onoff,:ki,:kd,:kp,:lower,:upper WHERE rowid=1;"
+ROW_UPDATE = "UPDATE setting SET target_temp = :target_temp, last_position = :last_position, \
+  onoff = :onoff, ki = :ki, kd = :kd, kp = :kp, lower = :lower, upper = :upper WHERE rowid=1;"
 app = Flask(__name__)
 
 def get_db():
