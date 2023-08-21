4dd969a9482615db3a3b36db022ab46ee499a829
Clam- <clam@nyanya.org>
2023-08-22 00:19:08 +1000

    Update updateDB.py
    My sqlite version doesn't support drop column ;_;

--- updateDB.py
+++ updateDB.py
@@ -1,11 +1,18 @@
 import sqlite3
+from tempcontrolloop import SETTING_DEFAULTS, TABLE_CREATE, ROW_CREATE
 
-ROW_ADD = "ALTER TABLE setting ADD COLUMN pos_margin REAL;"
+DATA_GET = "SELECT * from setting;"
 ROW_UPDATE = '''UPDATE setting SET pos_margin = ? WHERE rowid=1;'''
-MARGIN_DEFAULT = 50.0
+MARGIN_DEFAULT = 60
 settingsfile = "settings.sqlite"
+
 con = sqlite3.connect(settingsfile)
-con.execute(ROW_ADD)
+con.row_factory = sqlite3.Row
+data = con.execute(DATA_GET)
+data = SETTING_DEFAULTS | data
+con.execute("DROP TABLE setting;")
 con.commit()
-self.con.execute(ROW_UPDATE, (MARGIN_DEFAULT,))
+con.execute(TABLE_CREATE)
+con.commit()
+con.execute(ROW_CREATE, data)
 con.commit()
